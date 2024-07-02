6e3cd6403215826c788c0d9eeff28329f84c8aaf
logysnail <33370738+logysnail@users.noreply.github.com>
2024-07-02 14:26:47 +0800

    Update Batch_summary_gb.py

--- GBsequenceOperation/Batch_summary_gb.py
+++ GBsequenceOperation/Batch_summary_gb.py
@@ -1,6 +1,16 @@
-import seqmoder
 import os
 import Readgb
+
+
+def get_list(file_path):
+    """输入打开的文件，获得删除前后换行符，输出一个每行作为一个元素的列表"""
+    opened = open(file_path, "r", encoding="UTF-8")
+    clean_lines = []
+    for line in opened.readlines():
+        line = line.strip("\n")
+        clean_lines.append(line)
+    opened.close()
+    return clean_lines
 
 
 def batch_summary_gb(all_file_path):
@@ -8,7 +18,7 @@ def batch_summary_gb(all_file_path):
     for file in os.listdir(all_file_path):
         with open(f'{all_file_path}_summary/{file}.csv', "w") as summary_gb_file:
             try:
-                gb_records = seqmoder.get_list(f'{all_file_path}/{file}/GenBank/sequence.gb')
+                gb_records = get_list(f'{all_file_path}/{file}/GenBank/sequence.gb')
                 summary_list = Readgb.summary_undivided_gb(gb_records)
                 for summary in summary_list:
                     summary_gb_file.write(f'{summary}\n')
